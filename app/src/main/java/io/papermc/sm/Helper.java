c411f3f9372698580df7ec0b553f994df4ddbccb
pairnon <janpairnon@gmail.com>
2024-04-15 23:39:23 -0700

    Remove unused method

--- app/src/main/java/io/papermc/sm/Helper.java
+++ app/src/main/java/io/papermc/sm/Helper.java
@@ -1,16 +1,9 @@
 package io.papermc.sm;
 
-import org.bukkit.entity.Player;
-
-// Class containing various helper methods
 public class Helper {
     
     public static long getTicksFromSeconds(int seconds) {
         return (long)seconds * 20;
-    }
-
-    public static boolean checkPlayerExists(Player player) {
-        return (player != null && player.isOnline());
     }
 
 }
